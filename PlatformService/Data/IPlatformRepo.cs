132e202eefc352c5be8487ef82baff01a684b56d
ioakimansky_tn <ioakimansky_tn@shopandshow.ru>
2022-04-14 19:15:22 +0700

    Update IPlatformRepo.cs

--- PlatformService/Data/IPlatformRepo.cs
+++ PlatformService/Data/IPlatformRepo.cs
@@ -6,8 +6,11 @@ namespace PlatformService.Data
     public interface IPlatformRepo
     {
         bool SaveChanges();
+
         IEnumerable<Platform> GetAllPlatforms();
+
         Platform GetPlatformById(int id);
+
         void CreatePlatform(Platform plat);
         
     }
