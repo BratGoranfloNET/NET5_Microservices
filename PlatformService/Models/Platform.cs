2f3be38423a8245048dff7866f993a9e673642ef
ioakimansky_tn <ioakimansky_tn@shopandshow.ru>
2022-04-14 17:53:51 +0700

    add model

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -7,6 +7,9 @@ namespace PlatformService.Models
 {
     public class Platform
     {
-
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string Publisher { get; set; }
+        public string  Cost { get; set; }
     }
 }
