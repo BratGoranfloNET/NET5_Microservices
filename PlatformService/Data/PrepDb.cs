da5e7a7d5401ff93079896726ace08f2ae028672
ioakimansky_tn <ioakimansky_tn@shopandshow.ru>
2022-05-16 20:41:59 +0700

    PrepDb

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serverScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serverScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data");
+
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot NET",
+                        Publisher = "Soloncy",
+                        Cost = "Free"
+                    }
+                    );
+
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        } 
+
+    }
+}
